3a3b4b11dfa23065d77500f78fec40cad1da9769
Chavdar Rakov <chavdar@Chavdars-MacBook-Pro.local>
2020-09-11 17:00:35 +0300

    ref1

--- main.tf
+++ main.tf
@@ -5,15 +5,15 @@ resource "google_compute_instance" "server" {
   machine_type              = "f1-micro"
   allow_stopping_for_update = true
   tags                      = ["consul"]
-  count                     = "${var.server_count}"
+  count                     = var.server_count
   boot_disk {
     initialize_params {
-      image = "${var.gcp_image}"
+      image = var.gcp_image
     }
   }
 
   network_interface {
-    subnetwork = "${var.subnet-vm}"
+    subnetwork = var.subnet-vm
     network_ip = "172.31.${var.dc_net}.${count.index + 11}"
     access_config {
       // Ephemeral IP
@@ -24,7 +24,7 @@ resource "google_compute_instance" "server" {
     ssh-keys = "kayode:${file("${var.public_key_path}")}"
   }
 
-  metadata_startup_script = "${file("${path.module}/scripts/consul.sh")}"
+  metadata_startup_script = file("${path.module}/scripts/consul.sh")
 
   service_account {
     scopes = ["https://www.googleapis.com/auth/compute.readonly"]
--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "public_ip" {
-  value = "${google_compute_instance.server.*.network_interface.0.access_config.0.nat_ip}"
+  value = google_compute_instance.server.*.network_interface.0.access_config.0.nat_ip
 }
 output "private_ip" {
-  value = "${google_compute_instance.server.*.network_interface.0.network_ip}"
+  value = google_compute_instance.server.*.network_interface.0.network_ip
 }
